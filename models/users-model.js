8d3057b0e160ef6d10c6318d987f5ea4b931bd18
Guilherme-Vecchi <guivecchi12@gmail.com>
2020-09-18 19:58:01 -0600

    added .env, route /register, changed url route in server.js, added a couple functions in users-model.js

--- models/users-model.js
+++ models/users-model.js
@@ -1,16 +1,38 @@
 const db = require("../data/dbConfig")
 
 async function add(user){
-
+    const [id] = await db("Users").insert(user)
+    return findById(id)
 }
 
 function findBy(filter) {
-	return db("users")
+	return db("Users")
 		.select("id", "username", "password")
         .where(filter)
 }
 
+function findById(id) {
+	return db("Users")
+		.select("id", "username", "role_id")
+		.where({ id })
+		.first()
+}
+
+function getRolebyId(id){
+    const role = findById(id).role_id
+    return db("Users as u")
+        .join("Roles as r", "u.role_id", "r.id")
+        .where(role)
+        .select("r.name")
+}
+function findAll() {
+	return db("users").select("id", "role_id", "username", )
+}
+
 module.exports = {
     add,
-    findBy
+    findBy,
+    findById,
+    getRolebyId,
+    findAll
 }
