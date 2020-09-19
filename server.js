2d7c0a00f2c7766f0aeb3120d16ce9cf401b1967
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-18 20:14:17 -0400

    changed things we missed earlier

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express")
+const helmet = require("helmet")
 const cors = require("cors")
 const usersRouter = require("./routers/users-router")
 // import router
 
 const server = express()
+server.use(express.json())
+server.use(helmet())
 server.use(cors())
 
 server.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to our API"
-    })
+	res.json({
+		message: "Welcome to our API"
+	})
 })
 server.use('users', usersRouter);
 server.use((err, req, res, next) => {
