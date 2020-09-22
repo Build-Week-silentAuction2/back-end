f010c5a825201ab514ea5ff3ef3ed67e7cffb7b5
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-21 21:46:18 -0400

    got help from tl

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,12 +1,15 @@
-exports.seed = async function (knex) {
-  await knex("Roles").truncate()
-  await knex("Users").truncate()
-  await knex("Auctions").truncate()
-  await knex("Items").truncate()
-  await knex("Bids").truncate()
+// exports.seed = async function (knex) {
+//   await knex("Roles").truncate()
+//   await knex("Users").truncate()
+//   await knex("Auctions").truncate()
+//   await knex("Items").truncate()
+//   await knex("Bids").truncate()
+//   //await knex.raw('TRUNCATE TABLE Items, Users CASCADE')
+// }
 
 
-
-
-  //await knex.raw('TRUNCATE TABLE Items, Users CASCADE')
-}
+const cleaner = require("knex-cleaner"); exports.seed = function (knex) {
+  return cleaner.clean(knex, {
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"] // don't empty migration tables
+  });
+};
--- data/seeds/01_roles.js
+++ data/seeds/01_roles.js
@@ -1,4 +1,7 @@
 exports.seed = async function (knex) {
+  // put this instead
+  // return knex("users").del().then(function() {
+  //   return knex("users").insert([
   await knex("Roles").insert([
     { name: "Buyer" },
     { name: "Seller" }
