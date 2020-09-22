03a0e3510cffb91d50cea6808c114063b4009c97
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-21 21:57:02 -0400

    added cleaner to knex

--- data/seeds/02_users.js
+++ data/seeds/02_users.js
@@ -1,9 +1,16 @@
 const bcrypt = require("bcryptjs")
 
 exports.seed = async function (knex) {
-  await knex("Users").insert([
-    { role_id: 1, username: "buyer1", password: await bcrypt.hash("abc123", 14) },
-    { role_id: 2, username: "seller1", password: await bcrypt.hash("abc123", 14) },
-    { role_id: 1, username: "buyer2", password: await bcrypt.hash("abc123", 14) },
-  ])
+  return knex("Users").del().then(function () {
+    return knex("Users").insert([
+      { role_id: 1, username: "buyer1", password: "abc123" },
+      { role_id: 2, username: "seller1", password: "abc123" },
+      { role_id: 1, username: "buyer2", password: "abc123" },
+    ])
+  })
+  // await knex("Users").insert([
+  //   { role_id: 1, username: "buyer1", password: await bcrypt.hash("abc123", 14) },
+  //   { role_id: 2, username: "seller1", password: await bcrypt.hash("abc123", 14) },
+  //   { role_id: 1, username: "buyer2", password: await bcrypt.hash("abc123", 14) },
+  // ])
 }
