248b7d3a0643b5744e6bf96aa5e6deadc7a12a5c
Guilherme-Vecchi <guivecchi12@gmail.com>
2020-09-23 17:40:30 -0600

    adding items

--- routers/items-router.js
+++ routers/items-router.js
@@ -43,13 +43,31 @@ router.get("/seller/:id", async(req, res, next) =>{
 router.get("/auction/:id", async(req, res, next) =>{
     try{
         const auctionItems = await Items.findByAuction(req.params.id)
-        // console.log(auctionItems)
         if (!auctionItems) {
             res.status(404).json({
                 message: "Seller not found"
             })
         }
         res.json(auctionItems)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.post("/", async(req, res, next) => {
+    try{
+        if(!req.body || !req.name || !req.image || !req.description || !req.price || !req.seller_user_id || !req.auction_id){
+            return res.status(400).json({
+                message: "Not all data needed was provided. Make sure you include a name, image url, description, price, seller id and auction id"
+            })
+        }
+        else{
+            const newItem = {
+                name: req.body.name,
+                image: req.body.image
+            }
+        }
     }
     catch(err){
         next(err)
