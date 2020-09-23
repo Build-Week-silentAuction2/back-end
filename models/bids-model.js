a05e4a973d5dd98b36260e5dff49c8a85c4845f8
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-22 22:15:01 -0400

    started making helper functions

--- models/bids-model.js
+++ models/bids-model.js
@@ -1,17 +1,44 @@
 // import db
+const db = require("../data/dbConfig")
 
 // make helper methods
 
 // add bid
+async function insert(bid) {
+
+}
 
 // get bids
+function find() {
+    return db("Bids")
+}
 
 // get bid by id
+function findById(id) {
+
+}
 
 // git bids by item
+function findByItem(item_id) {
+
+}
 
 // delete bid
+function remove(id) {
+
+}
 
 // update bid
+async function update(id, bid) {
+
+}
 
 // export helper methods
+module.exports = {
+    insert,
+    find,
+    findById,
+    findByItem,
+    remove,
+    update
+}
