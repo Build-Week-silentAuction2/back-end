e3d37e14eae10a837ad55001a4df955282aaaa9c
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-22 21:57:24 -0400

    created bids-router.js, made comments in bids router and model

---
+++ models/bids-model.js
@@ -0,0 +1,17 @@
+// import db
+
+// make helper methods
+
+// add bid
+
+// get bids
+
+// get bid by id
+
+// git bids by item
+
+// delete bid
+
+// update bid
+
+// export helper methods
---
+++ routers/bids-router.js
@@ -0,0 +1,20 @@
+// import express
+// import bids model
+
+// make router
+
+// make endpoints
+
+// GET /bids
+
+// GET /bids/:id
+
+// GET /bids/:item_id
+
+// POST /bids
+
+// DELETE /bids/:id
+
+// PUT /bids/:id
+
+// export router
