12ec91d3b31b76d0902b69696e2df11f94a141fe
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-23 21:47:17 -0400

    cleaned up seed files, changed passwords to hashes, deleted db3 to try and avoid merge conflicts

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,13 +1,3 @@
-// exports.seed = async function (knex) {
-//   await knex("Roles").truncate()
-//   await knex("Users").truncate()
-//   await knex("Auctions").truncate()
-//   await knex("Items").truncate()
-//   await knex("Bids").truncate()
-//   //await knex.raw('TRUNCATE TABLE Items, Users CASCADE')
-// }
-
-
 const cleaner = require("knex-cleaner");
 exports.seed = function (knex) {
   return cleaner.clean(knex, {
--- data/seeds/01_roles.js
+++ data/seeds/01_roles.js
@@ -1,13 +1,8 @@
 exports.seed = async function (knex) {
-  // put this instead
   return knex("Roles").del().then(function () {
     return knex("Roles").insert([
       { name: "Buyer" },
       { name: "Seller" }
     ])
   })
-  // await knex("Roles").insert([
-  //   { name: "Buyer" },
-  //   { name: "Seller" }
-  // ])
 }
