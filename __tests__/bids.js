54c6ad76453bd6ec2bcc0961c533a6c9fe299057
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-24 21:07:08 -0400

    added scripts to package.json, started writing bids tests

---
+++ __tests__/bids.js
@@ -0,0 +1,20 @@
+// import supertest
+// import server
+// import db
+const supertest = require("supertest")
+const server = require("../server")
+const db = require("../data/dbConfig")
+
+// jest hooks
+
+// write tests
+
+describe("bids integration tests", () => {
+    it("GET /bids, gets all bids", async () => {
+        // make get request to /bids
+        // make assertions
+        // correct status code?
+        // correct data type?
+        // correct data?
+    })
+})
